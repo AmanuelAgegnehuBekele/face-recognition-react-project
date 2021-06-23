349552aa740ff50720be08c6ed3a2a2dbc5ce64a
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-06-23 11:53:02 +0300

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React,{ Component } from "react";
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 class App extends Component {
   render() {
     return(
       <div className="App">
         <Navigation />
+        <Logo />
+        <ImageLinkForm />
         {
-          // <Logo />
-          // <ImageLinkForm />
           // <FaceRecognition />
 
         }
