f566ce0986d7173b2a9a141f991dadebe1bd269c
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2021-07-01 11:19:22 +0300

    Final components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React,{ Component } from "react";
+
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank/>
         <ImageLinkForm />
         {
           // <FaceRecognition />
